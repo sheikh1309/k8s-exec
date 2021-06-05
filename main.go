a24764bfc9b3785abfddc87948ea84e332dd6a63
Hassan <hassan@moonshot.co.il>
2021-06-05 19:39:12 +0300

    new version 1.0.1

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "k8s-exec-pod/pkgs/command"
+import "github.com/sheikh1309/k8s-exec-pod/pkgs/command"
 
 func main()  {
-	command.SetExecutable("k8s-exec", "1.0.0", "This tool if for exec commands in deployment pods or in specific pod")
+	command.SetExecutable("k8s-exec", "1.0.1", "This tool if for exec commands in deployment pods or in specific pod")
 }
